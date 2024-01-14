f7070402aac7eae5e1059088e6e9416b8b07ba0e
AmanKrAman <aman41238@gmail.com>
2024-01-14 23:21:31 +0530

    coros fixed

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
 const PORT = process.env.PORT || 8080;
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://food-metro.vercel.app/');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://food-metro.vercel.app/');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 //mongodb connection
 mongoose.set("strictQuery", false);
 mongoose

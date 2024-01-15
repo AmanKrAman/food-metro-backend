238fde2f1a8e34e902a9a017d0d61f851245bea5
AmanKrAman <aman41238@gmail.com>
2024-01-15 19:16:07 +0530

    coros fixed

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {origin: "*"}
+));
 app.use(express.json({ limit: "10mb" }));
 
 const PORT = process.env.PORT || 8080;
@@ -132,3 +134,5 @@ app.get("/product",async(req,res)=>{
 
 //server is running
 app.listen(PORT, () => console.log("Server is running at port : " + PORT));
+ 
+module.exports = app;
